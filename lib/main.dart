7c2f27fb24ddfb248c1db9bb86deb4da15caf714
alvine2200 <alvinellavu@gmail.com>
2023-05-18 18:03:31 +0300

    wordpair installed

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,18 @@ void main() => runApp(MyApp());
 //class hello World with title and text center
 class MyApp extends StatelessWidget {
   @override
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: const Color.fromARGB(255, 140, 20, 40)),
+        // theme: ThemeData(primaryColor: const Color.fromARGB(255, 140, 20, 40)),
         home: Scaffold(
-          appBar: AppBar(title: const Text('WordPair Counter')),
-          body: const Center(child: Text('Hello World')),
-        ));
+      appBar: AppBar(
+          backgroundColor: Colors.purple[900],
+          centerTitle: true,
+          title: const Text(
+            'WordPair Counter',
+            style: TextStyle(color: Color.fromARGB(255, 243, 239, 239)),
+          )),
+      body: const Center(child: Text('Hello World')),
+    ));
   }
 }
