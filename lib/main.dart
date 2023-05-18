124860daae734fcf85f114e804de1c1977bb1d6b
alvine2200 <alvinellavu@gmail.com>
2023-05-18 17:28:44 +0300

    hello world with title centered

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+//class hello World with title and text center
 class MyApp extends StatelessWidget {
+  @override
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Text(
-      'Hello World',
-      style: TextStyle(fontSize: 30.5),
-    ));
+        theme: ThemeData(primaryColor: const Color.fromARGB(255, 140, 20, 40)),
+        home: Scaffold(
+          appBar: AppBar(title: const Text('WordPair Counter')),
+          body: const Center(child: Text('Hello World')),
+        ));
   }
 }
